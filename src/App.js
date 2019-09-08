015c34b4f6ce86c802f8393bdd223bf480fc2571
Faisal <hatilas1994@gmail.com>
2019-09-07 23:46:20 -0700

    basic boilerplate ready

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import Routers from './Routers';
-import {Provider} from 'react-redux';
+// import {Provider} from 'react-redux';
 // import store from './Components/Store/index';
 
 
 export default class App extends Component {
   render() {
     return (
-      <Provider >
+      // <Provider >
         <Routers />
-      </Provider>
+      // </Provider>
     )
   }
 }
